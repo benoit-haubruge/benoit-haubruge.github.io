da9c7048ad34a0ea8d17baf84e6f38b1eaa86f48
gautier kiss <gkgkgk395@gmail.com>
2023-06-19 23:13:53 +0200

    router working

--- src/components/pages/aboutMePage/components/navbar/NavBar.tsx
+++ src/components/pages/aboutMePage/components/navbar/NavBar.tsx
@@ -4,12 +4,13 @@ import './NavBar.scss'
 const NavBar: React.FC<any> = () => {
     return (
         <ul className='navbar'>
-            <li className='navbarLink'><a href="/#aboutMeSection">About me</a></li>
-            <li className='navbarLink'><a href="/#skillsSection">skills</a></li>
+            <li className='navbarLink'><a href="/#aboutMeSection">About Me</a></li>
+            <li className='navbarLink'><a href="/#skillsSection">Skills</a></li>
             <li className='navbarLink'><a href="/#formationSection">Education</a></li>
-            <li className='navbarLink'><a href="/#experienceSection">Experiences pro</a></li>
-            <li className='navbarLink'><a href="/#otherSection">Passion & loisirs</a></li>
-            <li className='navbarLink'><Link to="/visites">Visites d'entreprises</Link></li>
+            <li className='navbarLink'><a href="/#experienceSection">Experiences Pro</a></li>
+            <li className='navbarLink'><a href="/#otherSection">Passion & Loisirs</a></li>
+            <li className='navbarLink'><a href="/#futureSection">Atouts & Futur</a></li>
+            <li className='navbarLink'><Link to="/visites">Visites d'Entreprises</Link></li>
             <li className='navbarLink'><a href={require('./cv.pdf')} target='_blank' rel='noopener noreferrer'>CV</a></li>
             <li className='navbarLink'><a href={require('./../../tfe.pdf')} target='_blank' rel='noopener noreferrer'>TFE</a></li>
         </ul>
